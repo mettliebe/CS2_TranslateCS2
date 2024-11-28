90b278513c9b02ea6d68e2028bb571e83834c8bd
mettliebe <174098178+mettliebe@users.noreply.github.com>
2024-11-28 18:05:36 +0100

    AppendExportTypeDropDownItems

--- TranslateCS2.Mod/Services/Exports/ExportServiceDictionarySourceStrategy.cs
+++ TranslateCS2.Mod/Services/Exports/ExportServiceDictionarySourceStrategy.cs
@@ -49,7 +49,8 @@ internal class ExportServiceDictionarySourceStrategy : AExportServiceStrategy, I
     }
 
     private void AppendExportTypeDropDownItems(List<DropdownItem<string>> items) {
-        // TODO:
+        HashSet<string> added = [];
+        List<DropdownItem<string>> exportDropDownItems = [];
         IList<MyLocaleInfo> localeInfos = this.locManagerProvider.GetLocaleInfos();
         foreach (MyLocaleInfo localeInfo in localeInfos) {
             IList<IDictionarySource> sources = localeInfo.Sources;
@@ -63,37 +64,75 @@ internal class ExportServiceDictionarySourceStrategy : AExportServiceStrategy, I
 
 
 
-            {
-                // database.name = Game
-                IList<IDictionarySource> baseGameLocaleAssets =
+            // database.name = Game
+            IList<IDictionarySource> baseGameLocaleAssets =
                 localeAssets
                     .Where(item => LocaleAssetProvider.BuiltInBaseGamePredicate(item as LocaleAsset))
                     .ToList();
-            }
 
 
 
             {
                 // database.name = ParadoxMods
-                IList<IDictionarySource> onlineLocaleAssets =
-                localeAssets
-                    .Where(item => LocaleAssetProvider.ParadoxModsPredicate(item as LocaleAsset))
-                    .ToList();
                 // id can be extracted from subpath
                 // try to get name from modmanager?
                 // in case of .cok-files the name can be extracted from path (for region packs for example)
+                IList<IDictionarySource> onlineLocaleAssets =
+                    localeAssets
+                        .Where(item => LocaleAssetProvider.ParadoxModsPredicate(item as LocaleAsset))
+                        .ToList();
+                foreach (IDictionarySource onlineLocaleAsset in onlineLocaleAssets) {
+                    if (onlineLocaleAsset is not LocaleAsset asset) {
+                        continue;
+                    }
+                    string id = OtherModsLocFilesHelper.GetIdFromAssetSubPath(asset);
+                    string name = id;
+                    if (asset.path.EndsWith(".cok")) {
+                        name = asset.path.Substring(asset.path.LastIndexOf(StringConstants.ForwardSlashChar) + 1).Replace(".cok", String.Empty);
+                    } else if (Int32.TryParse(id, out int idInt)) {
+                        Colossal.PSI.Common.Mod? mod = OtherModsLocFilesHelper.GetModViaId(this.runtimeContainer, idInt);
+                        if (mod is not null) {
+                            Colossal.PSI.Common.Mod m = (Colossal.PSI.Common.Mod) mod;
+                            name = m.displayName;
+                        }
+                    }
+                    if (added.Contains(name)) {
+                        continue;
+                    }
+                    added.Add(name);
+                    exportDropDownItems.Add(DropDownItemsHelper.Create(id, name));
+                }
             }
 
 
 
             {
                 // database.name = User
-                IList<IDictionarySource> localLocaleAssets =
-                localeAssets
-                    .Where(item => LocaleAssetProvider.UserModsPredicate(item as LocaleAsset))
-                    .ToList();
                 // do not have an id
                 // see ExportServiceAssetStrategy.HandleExportTypeDropDownItemsForUserMods
+                IList<IDictionarySource> localLocaleAssets =
+                    localeAssets
+                        .Where(item => LocaleAssetProvider.UserModsPredicate(item as LocaleAsset))
+                        .ToList();
+                foreach (IDictionarySource localLocaleAsset in localLocaleAssets) {
+                    if (localLocaleAssets is not LocaleAsset asset) {
+                        continue;
+                    }
+                    string? name = OtherModsLocFilesHelper.GetNameFromAssetSubPath(asset);
+                    if (name is null) {
+                        continue;
+                    }
+                    Colossal.PSI.Common.Mod? mod = OtherModsLocFilesHelper.GetModViaName(this.runtimeContainer, name);
+                    if (mod is null) {
+                        continue;
+                    }
+                    Colossal.PSI.Common.Mod m = (Colossal.PSI.Common.Mod) mod;
+                    if (added.Contains(m.displayName)) {
+                        continue;
+                    }
+                    added.Add(m.displayName);
+                    exportDropDownItems.Add(DropDownItemsHelper.Create(m.displayName, m.displayName));
+                }
             }
 
 
@@ -101,26 +140,33 @@ internal class ExportServiceDictionarySourceStrategy : AExportServiceStrategy, I
             {
                 // all dictionary sources other than localeAsset
                 // belong to code-mods
-                IList<IDictionarySource> mods =
-                sources
-                    .Except(localeAssets)
-                    .ToList();
                 // online: should have an id, name can be gathered from modmanager
                 // local: do not have an id, extract name somehow
-                // or just work with the name as follows?
-
-
-
-                IList<string> modNamesTest =
+                // just work with the name as follows,
+                // its more convenient to filter within the export-method,
+                // instead of a to and fro
+                IList<IDictionarySource> mods =
+                    sources
+                        .Except(localeAssets)
+                        .ToList();
+                IList<string> modNames =
                     mods
                         .Select(s => s.GetType().Assembly.ManifestModule.ScopeName.Replace(".dll", String.Empty))
                         .Distinct()
                         .ToList();
+                foreach (string modName in modNames) {
+                    if (added.Contains(modName)) {
+                        continue;
+                    }
+                    added.Add(modName);
+                    exportDropDownItems.Add(DropDownItemsHelper.Create(modName, modName));
+                }
             }
-
-            // breakpoint
-            int i = 0;
         }
+        // DropDownItem.displayName is a LocalizedString that's id is displayed/used!!!
+        // LocalizedString itself does not implement IComparable
+        IOrderedEnumerable<DropdownItem<string>> ordered = exportDropDownItems.OrderBy(item => item.displayName.id);
+        items.AddRange(ordered);
 
     }
 
